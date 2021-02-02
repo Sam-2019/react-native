5bbdadfce2efe23d131bb4e993a4de2c8195c5af
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-02-02 15:13:52 +0000

    passing params to a previous screen

--- App.tsx
+++ App.tsx
@@ -1,31 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { Platform, StyleSheet, View, Text, Button } from 'react-native';
+import { Platform, StyleSheet, } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import DetailsScreen from './components/DetailsScreen'
-
-export interface Props {
-  navigation: any;
-}
-
-const HomeScreen: React.FC<Props> = ({ navigation }) => {
-  return (
-    <View style={{
-      flex: 1, alignItems: 'center', justifyContent: 'center'
-    }}>
-      <Text>
-        Home Screen
-      </Text>
-      <Button
-        title='Go to Details'
-        onPress={() => navigation.navigate('Details')}>
-      </Button>
-
-    </View>
-  )
-}
-
+import CreatePost from './components/CreatePost'
+import HomeScreen from './components/HomeScreen'
 
 const Stack = createStackNavigator()
 
@@ -39,14 +19,10 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name='Home' component={HomeScreen} options={{ title: 'Home' }} />
-        <Stack.Screen name='Details' component={DetailsScreen} options={{ title: 'Details' }} />
+        <Stack.Screen name='Details' component={DetailsScreen} options={{ title: 'Details' }} initialParams={{ itemId: 42 }} />
+        <Stack.Screen name='CreatePost' component={CreatePost} options={{ title: 'Create Post' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  }
-})
--- components/DetailsScreen.tsx
+++ components/DetailsScreen.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Platform, StyleSheet, View, Text, Button } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 export interface Props {
     navigation: any;
+    route: any;
 }
 
-const DetailsScreen: React.FC<Props> = ({ navigation }) => {
+const DetailsScreen: React.FC<Props> = ({ navigation, route }) => {
+    const { itemId, otherParam } = route.params
     return (
         <View style={{
             flex: 1, alignItems: 'center', justifyContent: 'center'
@@ -13,11 +15,40 @@ const DetailsScreen: React.FC<Props> = ({ navigation }) => {
             <Text>
                 Details Screen
         </Text>
-            <Button title='Go to Home' onPress={
+
+            <Text>
+                itemId: {
+                    JSON.stringify(itemId)
+                }
+            </Text>
+
+            <Text>
+                otherParam: {
+                    JSON.stringify(otherParam)
+                }
+            </Text>
+
+            <Button title='Go to Details....again' onPress={
+                () => {
+                    navigation.push('Details', {
+                        itemId: Math.floor(Math.random() * 100)
+                    })
+                }
+            } />
+
+            {/* <Button title='Go to Home' onPress={
                 () => {
                     navigation.navigate('Home')
                 }
-            } />
+            } /> */}
+            <Button
+                title='Go back to first screen in stack'
+                onPress={() => navigation.popToTop()}>
+            </Button>
+            {/* <Button
+                title='Go back'
+                onPress={() => navigation.goBack()}>
+            </Button> */}
         </View>
     )
 }
