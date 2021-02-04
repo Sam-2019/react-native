5df47fe10774b20d1476842bcdf4e7b7cd250c0d
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-02-04 00:18:16 +0000

    updated App

--- App.tsx
+++ App.tsx
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { Platform, StyleSheet, } from 'react-native';
+import { Image, Platform, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import DetailsScreen from './components/DetailsScreen'
 import CreatePost from './components/CreatePost'
 import HomeScreen from './components/HomeScreen'
 
+
 const Stack = createStackNavigator()
+
+// function LogoTitle() {
+//   return(
+//     <Image style={{width:50, height:50}} source
+//     />
+//   )
+// }
+
+// function Title() {
+//   return (
+//     <Text style={{ width: 50, height: 50 }}>  Hello </Text>
+//   )
+// }
 
 export default function App() {
 
@@ -17,7 +31,16 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName='Home' screenOptions={{
+        headerStyle: {
+          backgroundColor: 'tomato'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold'
+        }
+      }}
+      >
         <Stack.Screen name='Home' component={HomeScreen} options={{ title: 'Home' }} />
         <Stack.Screen name='Details' component={DetailsScreen} options={{ title: 'Details' }} initialParams={{ itemId: 42 }} />
         <Stack.Screen name='CreatePost' component={CreatePost} options={{ title: 'Create Post' }} />
