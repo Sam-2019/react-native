f1751111e99e6a6218bf28398f478ba842067398
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-02-05 12:14:24 +0000

    updating code for a Notepad app

--- App.tsx
+++ App.tsx
@@ -1,51 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
+
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import DetailsScreen from './components/DetailsScreen'
-import CreatePost from './components/CreatePost'
-import HomeScreen from './components/HomeScreen'
+import { Image, Platform, StyleSheet, Text, View, Animated, Easing, TouchableWithoutFeedback } from 'react-native';
 
-
-const Stack = createStackNavigator()
-
-// function LogoTitle() {
-//   return(
-//     <Image style={{width:50, height:50}} source
-//     />
-//   )
-// }
-
-// function Title() {
-//   return (
-//     <Text style={{ width: 50, height: 50 }}>  Hello </Text>
-//   )
-// }
 
 export default function App() {
+  let scaleValue = new Animated.Value(0);
 
-  if (Platform.Version === 25) {
-    console.log('Running on Nougat')
-  }
+  const cardScale = scaleValue.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [1, 1.1, 1.2]
+  });
+
+  let transformStyle = { ...styles.container, transform: [{ scale: cardScale }] };
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home' screenOptions={{
-        headerStyle: {
-          backgroundColor: 'tomato'
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold'
-        }
-      }}
-      >
-        <Stack.Screen name='Home' component={HomeScreen} options={{ title: 'Home' }} />
-        <Stack.Screen name='Details' component={DetailsScreen} options={{ title: 'Details' }} initialParams={{ itemId: 42 }} />
-        <Stack.Screen name='CreatePost' component={CreatePost} options={{ title: 'Create Post' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <TouchableWithoutFeedback onPress={() => {
+      scaleValue.setValue(0);
+      Animated.timing(scaleValue, {
+        toValue: 1,
+        duration: 250,
+        easing: Easing.linear,
+        useNativeDriver: true
+      }).start();
+    }} onPressOut={() => {
+      Animated.timing(scaleValue, {
+        toValue: 0, // reset the animated value to 0
+        duration: 100, // animate over 100ms
+        easing: Easing.linear,
+        useNativeDriver: true
+      }).start();
+    }}>
+      <Animated.View style={transformStyle}>
+        <Image
+          style={{
+            resizeMode: "contain",
+            height: 100,
+            width: 200
+          }}
+          fadeDuration={300}
+          source={require("./logo.png")}
+        />
+      </Animated.View>
+    </TouchableWithoutFeedback>
+
+
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  }
+});
---
+++ components/BottomTabsNavigator.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/stack'
+
+const BottomTabs = createBottomTabNavigator()
+const BottomTabsNavigator = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default BottomTabsNavigator
