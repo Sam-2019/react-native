57964e42243e678f373a13204df2648fe8a47ea6
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-02-08 23:15:21 +0000

    added Bottom Navigation

--- App.tsx
+++ App.tsx
@@ -1,16 +1,62 @@
 
 import * as React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import Crypto from './Projects/Crypto/Crypto'
 
+function HomeScreen() {
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.head}>Cryptocurrencies</Text>
+        <Crypto />
+      </View>
+    </ScrollView>
+
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.head}>Cryptocurrencies</Text>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-      <Crypto />
-    </View>
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name="md-checkmark-circle" size={32} color="green" />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarBadge: 3 }} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -28,3 +74,4 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   }
 });
+
