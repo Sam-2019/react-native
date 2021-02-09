6ba93c2fbd3a56bddc2e7fd3eaa68ae10661aff9
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-02-09 16:55:09 +0000

    adjusted screen view and added width sizes for uniform view

--- Projects/Crypto/Crypto.tsx
+++ Projects/Crypto/Crypto.tsx
@@ -7,33 +7,45 @@ const Crypto = () => {
     return (
         <View style={styles.container}>
             <View style={styles.list}>
-                <Text style={styles.name}>Name</Text>
-                <Text style={styles.symbol}>Symbol</Text>
-                <Text style={styles.price}>Price (US$)</Text>
+
+                <View style={styles.name}>
+                    <Text> Name</Text>
+                </View>
+
+                <View style={styles.symbol}>
+                    <Text >Symbol</Text>
+                </View>
+
+                <View style={styles.price_usd}>
+                    <Text >Price (US$)</Text>
+                </View>
+
             </View>
-            <FlatListBasics2 />
+
+            <FlatListBasics />
+            
         </View>
     )
 }
 
 export default Crypto
 
-// const FlatListBasics = () => {
-//     return (
-//         <View>
-//             <FlatList
-//                 data={[
-//                     { key: 1, name: 'Bitcoin', symbol: 'BTC', price: 16735.96 },
-//                     { key: 2, name: 'Bitcoin Cash', symbol: 'BT Cash', price: 506.64 },
-//                     { key: 3, name: 'Etherium', symbol: 'ETH', price: 0.521819 },
-//                     { key: 4, name: 'Ripple', symbol: 'XRP', price: 1.00 },
-//                     { key: 5, name: 'DogeCoin', symbol: 'DC', price: 2.0 },
-//                 ]}
-//                 renderItem={({ item }) => <CryptoItem  {...item} />}
-//             />
-//         </View>
-//     );
-// }
+const FlatListBasics = () => {
+    return (
+        <View>
+            <FlatList
+                data={[
+                    { key: '1', name: 'Bitcoin', symbol: 'BTC', price_usd: '16735.96' },
+                    { key: '2', name: 'Bitcoin Cash', symbol: 'BT Cash', price_usd: ' 506.64' },
+                    { key: '3', name: 'Etherium', symbol: 'ETH', price_usd: '0.521819' },
+                    { key: '4', name: 'Ripple', symbol: 'XRP', price_usd: '1.00' },
+                    { key: '5', name: 'DogeCoin', symbol: 'DC', price_usd: '2.0' },
+                ]}
+                renderItem={({ item }) => <CryptoItem  {...item} />}
+            />
+        </View>
+    );
+}
 
 export interface Props {
     id?: number;
@@ -41,45 +53,45 @@ export interface Props {
     releaseYear?: string;
 }
 
-const FlatListBasics2: React.FC<Props> = (props) => {
-    const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
+// const FlatListBasics2: React.FC<Props> = (props) => {
+//     const [isLoading, setLoading] = useState(true);
+//     const [data, setData] = useState([]);
 
-    useEffect(() => {
+//     useEffect(() => {
 
-        async function getUser() {
-            try {
-                const response = await axios({
-                    method: 'post',
-                    url: 'https://api.coinlore.net/api/tickers/',
-                })
-                // console.log((response.data.data))
-                setLoading(false);
-                setData((response.data.data))
-            } catch (error) {
-                console.error(error);
-            }
-        }; getUser()
-    }, [])
+//         async function getUser() {
+//             try {
+//                 const response = await axios({
+//                     method: 'post',
+//                     url: 'https://api.coinlore.net/api/tickers/',
+//                 })
+//                 console.log((response.data.data))
+//                 setLoading(false);
+//                 setData((response.data.data))
+//             } catch (error) {
+//                 console.error(error);
+//             }
+//         }; getUser()
+//     }, [])
 
 
-    return (
-        <View style={styles.container}>
-            {isLoading ? <ActivityIndicator /> : (
-                <FlatList
-                    data={data}
-                    keyExtractor={({ id }, index) => id}
-                    renderItem={({ item }) => <CryptoItem  {...item} />}
-                />
-            )}
-        </View>
-    );
-}
+//     return (
+//         <View style={styles.container}>
+//             {isLoading ? <ActivityIndicator /> : (
+//                 <FlatList
+//                     data={data}
+//                     keyExtractor={({ id }, index) => id}
+//                     renderItem={({ item }) => <CryptoItem  {...item} />}
+//                 />
+//             )}
+//         </View>
+//     );
+// }
 
 const styles = StyleSheet.create({
     container: {
         borderRadius: 5,
-        // paddingBottom: 10,
+        paddingBottom: 10,
     },
     item: {
         padding: 10,
@@ -91,22 +103,16 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
         borderRadius: 5,
-        padding: 10,
-    },
-    logoXname: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        paddingTop: 10,
+        paddingBottom: 10,
     },
     name: {
-        borderColor: 'red',
-        borderWidth: 1,
+        width: 120
     },
     symbol: {
-        borderColor: 'red',
-        borderWidth: 1,
+        width: 60
     },
-    price: {
-        borderColor: 'red',
-        borderWidth: 1,
+    price_usd: {
+        width: 70,
     }
 });
--- Projects/Crypto/crypto-item.tsx
+++ Projects/Crypto/crypto-item.tsx
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export interface Props {
     name: string;
-    symbol?: string;
-    price_usd?: string;
+    symbol: string;
+    price_usd: string;
 }
 
 const CryptoItem: React.FC<Props> = ({ name, symbol, price_usd }) => {
@@ -12,12 +12,20 @@ const CryptoItem: React.FC<Props> = ({ name, symbol, price_usd }) => {
         <View style={styles.item}>
 
             <View style={styles.logoXname}>
-                <Text style={styles.logo}> Hello </Text>
-                <Text style={styles.name}> {name} </Text>
+                <Text style={styles.logo}> Hell</Text>
+                <Text style={styles.name}> {name}</Text>
             </View>
 
-            <Text style={styles.symbol}>{symbol}</Text>
-            <Text style={styles.price}>${price_usd}</Text>
+            <View style={styles.symbol}>
+                <Text>{symbol}</Text>
+            </View>
+
+            <View style={styles.price}>
+                <Text>${price_usd} </Text>
+            </View>
+
+
+
         </View>
     )
 }
@@ -27,30 +35,28 @@ export default CryptoItem
 const styles = StyleSheet.create({
     item: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
         alignItems: 'center',
+        justifyContent: 'space-between',
         borderColor: 'gray',
         borderWidth: 1,
         borderRadius: 5,
         paddingTop: 10,
         paddingBottom: 10,
-        marginBottom: 7
+        marginBottom: 7,
     },
     logoXname: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        width: 120,
     },
     logo: {
-
     },
     name: {
-        fontWeight: '600'
     },
     symbol: {
-        borderColor: 'red',
-        borderWidth: 1,
+        width: 60,
     },
     price: {
-        fontWeight: '600'
+        width: 70,
+        alignItems: 'flex-end',
     }
 });
