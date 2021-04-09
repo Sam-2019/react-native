7a324422b956a40c976371e267f5348a87afe133
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-09 21:11:18 +0000

    added Loading for Music

---
+++ Projects/Music/LoadingPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Animated, Text } from "react-native";
+import { usePulse } from "./usePulse";
+import { styles } from "./styles";
+
+const Loading = () => {
+  const Scale = usePulse();
+
+  return (
+    <Animated.View style={[{ transform: [{ scale: Scale }] }]}>
+      <Text style={styles.text}>Music</Text>
+    </Animated.View>
+  );
+};
+export default Loading;
