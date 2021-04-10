e42b5c8ec5fe438b3694cfd2f866bf6db33a36c3
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-10 20:17:30 +0000

    imported File

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import * as React from "react";
 import AppFile from "./Projects/Trial/AppFile";
 import Crypto from "./Projects/Crypto/Crypto";
 import Music from "./Projects/Music/Home";
+import File from './Projects/Music/Artist/File'
 
 const App = () => {
-  return <Music />;
+  return <File />;
 };
 
 export default App;
