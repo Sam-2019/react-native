1ce8b69cef9b47014c13cf38f96f139a983a07c6
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-11 23:23:34 +0000

    imported ClickItem

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import AppFile from "./Projects/Trial/AppFile";
 import Crypto from "./Projects/Crypto/Crypto";
 import Music from "./Projects/Music/Home";
-import File from './Projects/Music/Artist/File'
+import File from './Projects/Music/Artist/clickArtist'
 
 const App = () => {
   return <Music />;
