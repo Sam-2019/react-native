3024e9399b238230f94e8f614a03705112d12180
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-12 20:11:50 +0000

    added data, updated Layout & styles

--- Projects/Music/Layout.tsx
+++ Projects/Music/Layout.tsx
@@ -1,15 +1,73 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import Artists from "./Artist/Artist";
+import Artists from "./Artist/view";
+
+export const data = [
+  {
+    id: 1,
+    name: "J. Cole",
+    imageURL:
+      "https://media.gq.com/photos/5c8ef5e207a98f3722295cb8/16:9/w_1280,c_limit/j-cole-cover-gq-april-2019_social.jpg",
+  },
+  {
+    id: 2,
+    name: "Marley G",
+    imageURL:
+      "https://montrealdancehall.com/wp-content/uploads/2019/11/skipmarleynewpic-800x445.png",
+  },
+  {
+    id: 3,
+    name: "Drake",
+    imageURL:
+      "https://media.pitchfork.com/photos/5b6db9f7f37ea104bc2b60b5/2:1/w_790/Drake.jpg",
+  },
+  {
+    id: 4,
+    name: "J. Cole",
+    imageURL:
+      "https://media.gq.com/photos/5c8ef5e207a98f3722295cb8/16:9/w_1280,c_limit/j-cole-cover-gq-april-2019_social.jpg",
+  },
+  {
+    id: 5,
+    name: "Marley G",
+    imageURL:
+      "https://montrealdancehall.com/wp-content/uploads/2019/11/skipmarleynewpic-800x445.png",
+  },
+  {
+    id: 6,
+    name: "Drake",
+    imageURL:
+      "https://media.pitchfork.com/photos/5b6db9f7f37ea104bc2b60b5/2:1/w_790/Drake.jpg",
+  },
+  {
+    id: 7,
+    name: "J. Cole",
+    imageURL:
+      "https://media.gq.com/photos/5c8ef5e207a98f3722295cb8/16:9/w_1280,c_limit/j-cole-cover-gq-april-2019_social.jpg",
+  },
+  {
+    id: 8,
+    name: "Marley G",
+    imageURL:
+      "https://montrealdancehall.com/wp-content/uploads/2019/11/skipmarleynewpic-800x445.png",
+  },
+  {
+    id: 9,
+    name: "Drake",
+    imageURL:
+      "https://media.pitchfork.com/photos/5b6db9f7f37ea104bc2b60b5/2:1/w_790/Drake.jpg",
+  },
+];
 
 const Layout = () => {
   return (
     <View style={styles.container}>
-      <Artists />
+      {data.map((item) => (
+        <Artists {...item} key={item.id} />
+      ))}
     </View>
   );
 };
-
 export default Layout;
 
 const styles = StyleSheet.create({
@@ -17,12 +75,8 @@ const styles = StyleSheet.create({
     flex: 1,
     flexWrap: "wrap",
     flexDirection: "row",
-  },
-  scrollView: {
-    backgroundColor: "pink",
-    marginHorizontal: 20,
-  },
-  text: {
-    fontSize: 42,
+    justifyContent: "center",
+    backgroundColor: 'red',
+    height: '100%'
   },
 });
